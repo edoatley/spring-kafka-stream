2dcb39c1a96afae7e8ddafc442b1c893a59fa080
edoatley <edoatley@gmail.com>
2025-02-08 12:31:08 +0000

    Getting started

---
+++ api/src/test/java/com/edoatley/sks/SpringKafkaStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edoatley.sks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
